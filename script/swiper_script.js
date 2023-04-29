5a9da6f6ac9b10e54b62ff433be36878285f464a
Pino0202 <pinochi0202@gmail.com>
2023-04-29 21:50:03 +0900

    いろいろ

--- script/swiper_script.js
+++ script/swiper_script.js
@@ -1,6 +1,10 @@
 const swiper = new Swiper(".swiper", {
     direction: "vertical",
     pagination: {
-        el: ".swiper-pagination"
-    }
+        el: ".swiper-pagination",
+    },
+    navigation: {
+        nextEl: '.swiper__next',
+        prevEl: '.swiper__prev',
+    },
 });
